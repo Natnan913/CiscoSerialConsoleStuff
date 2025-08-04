2af4c66473e3726de491665d8a817acdd5ecf700
Natnan913 <68988354+Natnan913@users.noreply.github.com>
2025-08-05 09:47:39 +1000

    Add files via upload
    There was a call for a variable name that needed updating.

--- RequestOutput-Cisco.py
+++ RequestOutput-Cisco.py
@@ -6,7 +6,7 @@ intendedSerialPort= serial.Serial('COM3', 9600, timeout=1)
 def outputOfCommand(command, wait=1) :
     intendedSerialPort.write(command.encode() + b'\r\n') #used \r\n so the Cisco switch can tell that where the end of the command is.
     time.sleep(wait)  #it needs to wait for output otherwise the command comes out incorrectly. Same reason why I've kind of spammed "time.sleep" below.  
-    output = ser.read(4096)               
+    output = intendedSerialPort.read(4096)               
     return output.decode(errors='ignore')
 
 versionInfo = outputOfCommand('show version')
